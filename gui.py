e780ec5cffbcd030e1cb1d0a36daeb6b12bd4788
Fanyu Meng <fanyu.meng98@gmail.com>
2018-01-09 20:10:02 +0800

    Enabled display config

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ import sys
 
 
 class CombinedWindow(QWidget):
-    def __init__(self):
+    def __init__(self, font, font_size, font_color):
         super().__init__()
         self.setGeometry(300, 300, 180, 310)
         self.setWindowTitle('Display')
@@ -24,21 +24,23 @@ class CombinedWindow(QWidget):
 
         self._buttons_attacker = QPushButton('ATTACKERS:', self)
         self._buttons_attacker.clicked.connect(self.handle_button_attacker)
-        self._buttons_attacker.setFont((QFont('bebas neue', 24, QFont.Bold)))
-        self._buttons_attacker.setStyleSheet('QPushButton {background-color: rgba(255, 255, 255, 10); color: white;}')
+        self._buttons_attacker.setFont((QFont(font, font_size, QFont.Bold)))
+        self._buttons_attacker.setStyleSheet('QPushButton {background-color: rgba(255, 255, 255, 35); color: '
+                                             + font_color + ';}')
 
         self._labels_attacker = QLabel(self._text_attackers, self)
-        self._labels_attacker.setFont(QFont('bebas neue', 24, QFont.Bold))
-        self._labels_attacker.setStyleSheet('color: white')
+        self._labels_attacker.setFont(QFont(font, font_size, QFont.Bold))
+        self._labels_attacker.setStyleSheet('color: ' + font_color)
 
         self._buttons_defender = QPushButton('DEFENDERS:', self)
         self._buttons_defender.clicked.connect(self.handle_button_defender)
-        self._buttons_defender.setFont((QFont('bebas neue', 24, QFont.Bold)))
-        self._buttons_defender.setStyleSheet('QPushButton {background-color: rgba(255, 255, 255, 10); color: white;}')
+        self._buttons_defender.setFont((QFont(font, font_size, QFont.Bold)))
+        self._buttons_defender.setStyleSheet('QPushButton {background-color: rgba(255, 255, 255, 35); color: '
+                                             + font_color + ';}')
 
         self._labels_defender = QLabel(self._text_defenders, self)
-        self._labels_defender.setFont(QFont('bebas neue', 24, QFont.Bold))
-        self._labels_defender.setStyleSheet('color: white')
+        self._labels_defender.setFont(QFont(font, font_size, QFont.Bold))
+        self._labels_defender.setStyleSheet('color: ' + font_color)
 
         grid.addWidget(self._buttons_attacker, 0, 0)
         grid.addWidget(self._labels_attacker, 1, 0)
@@ -72,9 +74,9 @@ class CombinedWindow(QWidget):
 
 
 class GUI:
-    def __init__(self, button_attacker_fn, button_defender_fn):
+    def __init__(self, button_attacker_fn, button_defender_fn, font):
         self.app = QApplication(sys.argv)
         # TODO： implement button handler
         CombinedWindow.button_attacker = button_attacker_fn
         CombinedWindow.button_defender = button_defender_fn
-        self.combined_window = CombinedWindow()
+        self.combined_window = CombinedWindow(font[0], font[1], font[2])
